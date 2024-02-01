cd3496bdffd8de8c6724bfa518bf132e92577726
Bigto Chan <taitocdt@gmail.com>
2024-02-01 16:34:50 +0800

    Feature/compare all results (#14)
    * chore(lib): Update lib versions
    
    * feat(monitor): Sort and compare all results
    
    * chore(log): Print more info
    
    * chore(test): Update testcases
    
    * chore(config): Update config values

--- src/monitors/monitor_cex.rs
+++ src/monitors/monitor_cex.rs
@@ -7,7 +7,8 @@ use crate::exchanges::{
 
 };
 use crate::utils::config_struct::{ExchangeDifference, Exchanges};
-use crate::utils::number_utils::find_lowest_price_result;
+use tuple_conv::RepeatedTuple;
+use crate::utils::number_utils::sort_price_result;
 
 pub async fn exchange_prices(exchange_difference: ExchangeDifference) {
     let okx = OkxActor::new();
@@ -23,25 +24,31 @@ pub async fn exchange_prices(exchange_difference: ExchangeDifference) {
             mexc.fetch_price(instrument.clone(), find_exchange_config(exchange_difference.exchanges.clone(), "MEXC")),
         );
 
-        let (okx_result, hashkey_result, mexc_result) = all_results;
+        let sorted_results = sort_price_result(all_results.clone().to_vec());
 
         // okx.fetch_ccy_data(instrument.clone(), find_exchange_config(exchange_difference.exchanges.clone(), "OKX")).await.expect("OKX: panic message");
         // mexc.fetch_ccy_info(instrument.clone(), find_exchange_config(exchange_difference.exchanges.clone(), "MEXC")).await.expect("MEXC: panic message");
-        // hashkey.fetch_account(instrument.clone(), find_exchange_config(exchange_difference.exchanges.clone(), "HashKey")).await.expect("MEXC: panic message");
+        // hashkey.fetch_account(instrument.clone(), find_exchange_config(exchange_difference.exchanges.clone(), "HashKey")).await.expect("HashKey: panic message");
 
-        println!("OKX: {:?}", okx_result);
-        println!("HashKey: {:?}", hashkey_result);
-        println!("MEXC: {:?}", mexc_result);
+        println!("\nAll results: ");
+        println!("☉ OKX → {:?}", all_results.clone().0.unwrap());
+        println!("☉ HashKey → {:?}", all_results.clone().1.unwrap());
+        println!("☉ MEXC → {:?}", all_results.clone().2.unwrap());
 
-        let lowest_result = find_lowest_price_result(vec!(okx_result.unwrap(), mexc_result.unwrap()));
+        let highest_result = sorted_results.get(0).unwrap();
+        let lowest_result = sorted_results.get(sorted_results.len() - 1).unwrap();
 
-        println!("The lowest price is {:?}", lowest_result);
+        println!("\nSorted results: ");
+        println!("☉ The highest → {:?}", highest_result);
+        println!("☉ The lowest → {:?}", lowest_result);
 
-        let target_exchange_price = hashkey_result.unwrap().clone().price;
-        let price_difference = target_exchange_price - lowest_result.price;
-        let percent_rate = price_difference / target_exchange_price;
+        let price_difference = highest_result.price - lowest_result.price;
+        let percent_rate = price_difference / highest_result.price;
         let percent_str = percent_rate * 100_f32;
-        println!("{} vs HashKey ({}) => [Difference: {price_difference}], [percent: {percent_str}%] \n", lowest_result.data_source, lowest_result.instrument)
+        println!(
+            "\nHighest {} 🆚 lowest {} ({}) => [Difference: {price_difference}], [percent: {percent_str}%] \n",
+            highest_result.data_source, lowest_result.data_source, lowest_result.instrument
+        )
     }
 }
 
